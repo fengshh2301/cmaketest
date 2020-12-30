37b30f558a94298f25fa4b56dbed968a6be712e7
fengshh2301 <fengshh2301@sina.com>
2020-12-30 22:15:51 +0800

    windows生成dll及其lib接口库

--- add_dynamic/include/add_dy.h
+++ add_dynamic/include/add_dy.h
@@ -1,6 +1,12 @@
 #ifndef ADD_DY_H
 #define ADD_DY_H
 
-int add_dy_func(int a, int b);
+#if(defined WIN32||defined_WIN32|| defined WINCE)&& defined EFIMPL
+#define EXPORT __declspec(dllexport)
+#else
+#define EXPORT
+#endif
+
+EXPORT int add_dy_func(int a, int b);
 
 #endif // !ADD_DY_H
