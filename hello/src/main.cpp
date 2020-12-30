6faa36f06d9132e7bb87da1ae666f76f1725e44a
fengshh2301 <fengshh2301@sina.com>
2020-12-30 14:59:00 +0800

    b.引用工程代码示例

--- hello/src/main.cpp
+++ hello/src/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include "add.h"
 
 int main(int argc, char** args)
 {
     std::cout << "hello world!" << std::endl;
+	std::cout << "5+6=" << add(5, 6) << std::endl;
     return 0;
 }
